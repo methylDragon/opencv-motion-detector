354358ae53ddb8e4c24218d5292dad8585ac0780
methylDragon <methylDragon@gmail.com>
2018-07-11 18:15:02 +0800

    Initial Commit

---
+++ Motion Detector.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+OpenCV Motion Detector
+@author: methylDragon
+
+                                   .     .
+                                .  |\-^-/|  .
+                               /| } O.=.O { |\
+                              /´ \ \_ ~ _/ / `\
+                            /´ |  \-/ ~ \-/  | `\
+                            |   |  /\\ //\  |   |
+                             \|\|\/-""-""-\/|/|/
+                                     ______/ /
+                                     '------'
+                       _   _        _  ___
+             _ __  ___| |_| |_ _  _| ||   \ _ _ __ _ __ _ ___ _ _
+            | '  \/ -_)  _| ' \ || | || |) | '_/ _` / _` / _ \ ' \
+            |_|_|_\___|\__|_||_\_, |_||___/|_| \__,_\__, \___/_||_|
+                               |__/                 |___/
+            -------------------------------------------------------
+                           github.com/methylDragon
+
+References/Adapted From:
+https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
+
+Description:
+This script runs a motion detector! It detects transient motion in a room
+and said movement is large enough, and recent enough, reports that there is
+motion!
+
+Run the script with a working webcam! You'll see how it works!
+"""
+
+import imutils
+import cv2
+
+
+# =============================================================================
+# USER-SET PARAMETERS
+# =============================================================================
+
+# Number of frames to pass before changing the frame to compare the current
+# frame against
+FRAMES_TO_PERSIST = 10
+
+# Minimum boxed area for a detected motion to count as actual motion
+# Use to filter out noise or small objects
+MIN_SIZE_FOR_MOVEMENT = 2000
+
+# Minimum length of time where no motion is detected it should take
+#(in program cycles) for the program to declare that there is no movement
+MOVEMENT_DETECTED_PERSISTENCE = 100
+
+# =============================================================================
+# CORE PROGRAM
+# =============================================================================
+
+
+# Create capture object
+cap = cv2.VideoCapture(5) # Flush the stream
+cap.release()
+cap = cv2.VideoCapture(0) # Then start the webcam
+
+# Init frame variables
+first_frame = None
+next_frame = None
+
+# Init display font and timeout counters
+font = cv2.FONT_HERSHEY_SIMPLEX
+delay_counter = 0
+movement_persistent_counter = 0
+
+# LOOP!
+while True:
+
+    # Set transient motion detected as false
+    transient_movement_flag = False
+    
+    # Read frame
+    ret, frame = cap.read()
+    text = "Unoccupied"
+
+    # If there's an error in capturing
+    if not ret:
+        print("CAPTURE ERROR")
+        continue
+
+    # Resize and save a greyscale version of the image
+    frame = imutils.resize(frame, width = 750)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Blur it to remove camera noise (reducing false positives)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # If the first frame is nothing, initialise it
+    if first_frame is None: first_frame = gray    
+
+    delay_counter += 1
+
+    # Otherwise, set the first frame to compare as the previous frame
+    # But only if the counter reaches the appriopriate value
+    # The delay is to allow relatively slow motions to be counted as large
+    # motions if they're spread out far enough
+    if delay_counter > FRAMES_TO_PERSIST:
+        delay_counter = 0
+        first_frame = next_frame
+
+        
+    # Set the next frame to compare (the current frame)
+    next_frame = gray
+
+    # Compare the two frames, find the difference
+    frame_delta = cv2.absdiff(first_frame, next_frame)
+    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    # Fill in holes via dilate(), and find contours of the thesholds
+    thresh = cv2.dilate(thresh, None, iterations = 2)
+    _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # loop over the contours
+    for c in cnts:
+
+        # Save the coordinates of all found contours
+        (x, y, w, h) = cv2.boundingRect(c)
+        
+        # If the contour is too small, ignore it, otherwise, there's transient
+        # movement
+        if cv2.contourArea(c) > MIN_SIZE_FOR_MOVEMENT:
+            transient_movement_flag = True
+            
+            # Draw a rectangle around big enough movements
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # The moment something moves momentarily, reset the persistent
+    # movement timer.
+    if transient_movement_flag == True:
+        movement_persistent_flag = True
+        movement_persistent_counter = MOVEMENT_DETECTED_PERSISTENCE
+
+    # As long as there was a recent transient movement, say a movement
+    # was detected    
+    if movement_persistent_counter > 0:
+        text = "Movement Detected " + str(movement_persistent_counter)
+        movement_persistent_counter -= 1
+    else:
+        text = "No Movement Detected"
+
+    # Print the text on the screen, and display the raw and processed video 
+    # feeds
+    cv2.putText(frame, str(text), (10,35), font, 0.75, (255,255,255), 2, cv2.LINE_AA)
+    cv2.imshow("frame", frame)
+    cv2.imshow("delta", frame_delta)
+
+    # Interrupt trigger by pressing q to quit the open CV program
+    ch = cv2.waitKey(1)
+    if ch & 0xFF == ord('q'):
+        break
+
+# Cleanup when closed
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cap.release()
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # opencv-motion-detector
-Detects transient motion in a room and said movement is large enough, and recent enough, reports that there is motion!
+Detects transient motion in a room. If said movement is large enough, and recent enough, reports that there is motion!
+
+
+
+The motion detected state is then held for some user specified amount of time even when no motion is detected until finally the program declares that no motion is detected.
+
+
+
+**Dependencies**
+
+- OpenCV 2
+- Image Utilities (imutils)
+
+
+
+**User Configurable Parameters**
+
+Just change these few parameters located near the top of the Python script
+
+- FRAMES_TO_PERSIST
+  -  Number of frames to pass before changing the frame to compare the current frame against.
+- MIN_SIZE_FOR_MOVEMENT
+  - Minimum boxed area for a detected motion to count as actual motion. Use it to filter out noise or small objects.
+- MOVEMENT_DETECTED_PERSISTENCE
+  - Minimum length of time where no motion is detected it should take (in program cycles) for the program to declare that there is no movement.
